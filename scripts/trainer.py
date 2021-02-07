6bab6862942de406e65d171090b4b1850db465f1
A6a7H <rosteshka@gmail.com>
2021-02-08 00:41:38 +0300

    fix

--- scripts/trainer.py
+++ scripts/trainer.py
@@ -10,6 +10,14 @@ import matplotlib.pyplot as plt
 from IPython.display import clear_output
 
 
+lambda_pt = (
+    lambda i: 20
+    / np.pi
+    * 2
+    * torch.atan(torch.tensor(i, dtype=torch.float32, device=device) / 1e4)
+)
+
+
 class BaseTrainer(ABC):
     def __init__(self):
         pass
@@ -50,7 +58,7 @@ class RichNodeTrainer(BaseTrainer):
         generator_iteration: int = 1,
         weights_exist: bool = True,
         device: str = "cuda",
-        logger: Any = None
+        logger: Any = None,
     ):
         super(RichNodeTrainer, self).__init__()
         self.generator_model = generator_model
@@ -97,25 +105,25 @@ class RichNodeTrainer(BaseTrainer):
                 generated_2 = torch.cat([self.generator_model(noized_x), x], dim=1)
 
                 generator_loss = torch.mean(
-                    self.critic_loss(real_full_0, generated_2) * weight - 
-                    self.critic_loss(generated_1, generated_2) * weight
+                    self.critic_loss(real_full_0, generated_2) * weight
+                    - self.critic_loss(generated_1, generated_2) * weight
                 )
                 # epsilon = torch.rand(len(real), 1, 1, 1, device=device, requires_grad=True)
                 epsilon = torch.rand(real_full_0.shape[0], 1)
                 epsilon = epsilon.expand(real_full_0.size())
                 epsilon = epsilon.to(self.device)
 
-                gradient = get_gradient(self.critic_loss, real_full_0, generated_1, generated_2, epsilon)
+                gradient = get_gradient(
+                    self.critic_loss, real_full_0, generated_1, generated_2, epsilon
+                )
                 gp = gradient_penalty(gradient)
-                
-                critic_loss = 0.7 * gp - generator_loss
+
+                critic_loss = lambda_pt(epoch) * gp - generator_loss
                 critic_loss.backward()
                 self.discriminator_optimizer.step()
 
             if self.weights_exist:
-                x, weight, dlls = [
-                    i.to(self.device) for i in next(self.train_loader)
-                ]
+                x, weight, dlls = [i.to(self.device) for i in next(self.train_loader)]
             else:
                 x, dlls = [i.to(self.device) for i in next(self.train_loader)]
                 weight = torch.ones((x.shape[0]), device=self.device)
@@ -125,28 +133,35 @@ class RichNodeTrainer(BaseTrainer):
             self.generator_optimizer.zero_grad()
 
             noized_x = torch.cat(
-                    [x, get_noise(x.shape[0], self.z_dimensions).to(self.device)],
-                    dim=1,
-                )
+                [x, get_noise(x.shape[0], self.z_dimensions).to(self.device)],
+                dim=1,
+            )
             real_full_0 = torch.cat([dlls, x], dim=1)
             generated_1 = torch.cat([self.generator_model(noized_x), x], dim=1)
             generated_2 = torch.cat([self.generator_model(noized_x), x], dim=1)
 
             generator_loss = torch.mean(
-                    self.generator_loss(real_full_0, generated_2) * weight - 
-                    self.generator_loss(generated_1, generated_2) * weight
-                )
+                self.generator_loss(real_full_0, generated_2) * weight
+                - self.generator_loss(generated_1, generated_2) * weight
+            )
 
             generator_loss.backward()
             self.generator_optimizer.step()
 
-            self.logger.log_metrics({'Generator loss': generator_loss.item(),
-                                    'Critic loss': critic_loss.item()},
-                                    step=epoch)
+            self.logger.log_metrics(
+                {
+                    "Generator loss": generator_loss.item(),
+                    "Critic loss": critic_loss.item(),
+                    "Gradient Norm": torch.norm(
+                        gradient.view(len(gradient), -1).norm(2, dim=1)
+                    ),
+                },
+                step=epoch,
+            )
 
             self.generator_scheduler.step()
             self.discriminator_scheduler.step()
-            
+
             if epoch % self.display_step == 0:
                 with torch.no_grad():
                     if self.weights_exist:
@@ -158,28 +173,31 @@ class RichNodeTrainer(BaseTrainer):
                         weight = torch.ones((x.shape[0]))
                     real_full_0 = torch.cat([dlls, x], dim=1)
                     noized_x = torch.cat(
-                            [x, get_noise(x.shape[0], self.z_dimensions).to(self.device)],
-                            dim=1,
-                        )
+                        [x, get_noise(x.shape[0], self.z_dimensions).to(self.device)],
+                        dim=1,
+                    )
                     generated = self.generator_model(noized_x)
                     self.draw(dlls, generated)
         self.logger.end()
 
-    def draw(self, dlls, generated, dll_name: List[str] = ["RichDLLe", "RichDLLk", "RichDLLmu", "RichDLLp", "RichDLLbt"]):
+    def draw(
+        self,
+        dlls,
+        generated,
+        dll_name: List[str] = [
+            "RichDLLe",
+            "RichDLLk",
+            "RichDLLmu",
+            "RichDLLp",
+            "RichDLLbt",
+        ],
+    ):
         clear_output(False)
         fig, axs = plt.subplots(3, 2, figsize=(15, 15))
-        for INDEX, ax in zip((0, 1, 2, 3, 4), axs.flatten()):
-            _, bins, _ = ax.hist(dlls[:, INDEX].cpu(),
-                                bins=100,
-                                label="data")
-            ax.hist(generated[:, INDEX].cpu(),
-                    bins=bins,
-                    label="generated",
-                    alpha=0.5)
+        for INDEX, ax in zip([i for i in range(len(dll_name))], axs.flatten()):
+            _, bins, _ = ax.hist(dlls[:, INDEX].cpu(), bins=100, label="data")
+            ax.hist(generated[:, INDEX].cpu(), bins=bins, label="generated", alpha=0.5)
             ax.legend()
             ax.set_title(dll_name[INDEX])
         self.logger.log_figure()
         plt.show()
-        
-
-        
