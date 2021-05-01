7611ac55a872bb7962060aa03beb98a652fc2cd7
A6a7H <rosteshka@gmail.com>
2021-05-01 14:56:19 +0300

    merged

--- models/wgan.py
+++ models/wgan.py
@@ -11,37 +11,38 @@ from sklearn.model_selection import train_test_split
 from catboost import CatBoostClassifier
 
 from .metrics import calculate_roc_auc
-from models import (get_gradient, 
-                    get_noise, 
-                    gradient_penalty)
-from models import (create_node_model, 
-                    create_fcn_model)
+from models import get_gradient, get_noise, gradient_penalty
+from models import create_node_model, create_fcn_model
 
 logger = logging.getLogger(__name__)
 
-class WGAN():
+
+class WGAN:
     def __init__(self, config: tp.Dict[str, tp.Any]):
         self.params = config
         self.create_generator()
         self.create_critic()
 
     def configure_optimizers(self):
-        if self.params['generator']['optimizer'] == 'adam':
+        if self.params["generator"]["optimizer"] == "adam":
             generator_optimizer = torch.optim.Adam(
                 self.generator_model.parameters(),
-                lr=self.params['generator']['learning_rate'],
-                weight_decay=self.params['generator']['weight_decay'],
-                betas=(self.params["generator"]["beta1"], self.params["generator"]["beta2"])
+                lr=self.params["generator"]["learning_rate"],
+                weight_decay=self.params["generator"]["weight_decay"],
+                betas=(
+                    self.params["generator"]["beta1"],
+                    self.params["generator"]["beta2"],
+                ),
             )
         else:
             raise NameError("Unknown optimizer name")
-        
-        if self.params['critic']['optimizer'] == 'adam':
+
+        if self.params["critic"]["optimizer"] == "adam":
             critic_optimizer = torch.optim.Adam(
                 self.critic_model.parameters(),
-                lr=self.params['critic']['learning_rate'],
-                weight_decay=self.params['critic']['weight_decay'],
-                betas=(self.params["critic"]["beta1"], self.params["critic"]["beta2"])
+                lr=self.params["critic"]["learning_rate"],
+                weight_decay=self.params["critic"]["weight_decay"],
+                betas=(self.params["critic"]["beta1"], self.params["critic"]["beta2"]),
             )
         else:
             raise NameError("Unknown optimizer name")
@@ -49,38 +50,49 @@ class WGAN():
         return generator_optimizer, critic_optimizer
 
     def save_models(self, path, epoch):
-        torch.save({
-            'epoch': epoch,
-            'generator_model_state_dict': self.generator_model.state_dict(),
-            'critic_model_state_dict': self.critic_model.state_dict(),
-            }, path)
+        torch.save(
+            {
+                "epoch": epoch,
+                "generator_model_state_dict": self.generator_model.state_dict(),
+                "critic_model_state_dict": self.critic_model.state_dict(),
+            },
+            path,
+        )
 
     def load_models(self, path):
         checkpoint = torch.load(path)
-        self.generator_model.load_state_dict(checkpoint['generator_model_state_dict'])
-        self.critic_model.load_state_dict(checkpoint['critic_model_state_dict'])
+        self.generator_model.load_state_dict(checkpoint["generator_model_state_dict"])
+        self.critic_model.load_state_dict(checkpoint["critic_model_state_dict"])
         return checkpoint
 
     def create_generator(self):
         if self.params["generator_architecture"].lower() == "node":
             logger.info("Creating NODE model")
             logger.info(f"params: \n{self.params['generator']}")
-            self.generator_model = create_node_model(self.params['generator']['params'], 
-                                    model_type=self.params['generator_model_type'])
+            self.generator_model = create_node_model(
+                self.params["generator"]["params"],
+                model_type=self.params["generator_model_type"],
+            )
         elif self.params["critic_architecture"].lower() == "fcn":
             logger.info("Creating FCN model")
             logger.info(f"params: \n{self.params['generator']}")
-            self.generator_model = create_fcn_model(self.params['generator']['params'], 
-                                    model_type=self.params['generator_model_type'])
+            self.generator_model = create_fcn_model(
+                self.params["generator"]["params"],
+                model_type=self.params["generator_model_type"],
+            )
         else:
             raise NameError(
-                "Unknown generator architecture: {}".format(self.params["generator_architecture"])
+                "Unknown generator architecture: {}".format(
+                    self.params["generator_architecture"]
+                )
             )
-        
-        generator_checkpoint_path = self.params['checkpoint_path']
+
+        generator_checkpoint_path = self.params["checkpoint_path"]
         if generator_checkpoint_path:
             generator_checkpoint = torch.load(generator_checkpoint_path)
-            self.generator_model.load_state_dict(generator_checkpoint["generator_model_state_dict"])
+            self.generator_model.load_state_dict(
+                generator_checkpoint["generator_model_state_dict"]
+            )
 
         self.generator_model.to(self.params["device"])
 
@@ -88,27 +100,35 @@ class WGAN():
         if self.params["critic_architecture"].lower() == "node":
             logger.info("Creating NODE model")
             logger.info(f"params: \n{self.params['critic']}")
-            self.critic_model = create_node_model(self.params['critic']['params'], 
-                                    model_type=self.params['critic_model_type'])
+            self.critic_model = create_node_model(
+                self.params["critic"]["params"],
+                model_type=self.params["critic_model_type"],
+            )
         elif self.params["critic_architecture"].lower() == "fcn":
             logger.info("Creating FCN model")
             logger.info(f"params: \n{self.params['critic']}")
-            self.critic_model = create_fcn_model(self.params['critic']['params'], 
-                                    model_type=self.params['critic_model_type'])
+            self.critic_model = create_fcn_model(
+                self.params["critic"]["params"],
+                model_type=self.params["critic_model_type"],
+            )
         else:
             raise NameError(
-                "Unknown critic architecture: {}".format(self.params['critic_architecture'])
+                "Unknown critic architecture: {}".format(
+                    self.params["critic_architecture"]
+                )
             )
 
         critic_checkpoint_path = self.params["checkpoint_path"]
         if critic_checkpoint_path:
             critic_checkpoint = torch.load(critic_checkpoint_path)
-            self.critic_model.load_state_dict(critic_checkpoint["critic_model_state_dict"])
+            self.critic_model.load_state_dict(
+                critic_checkpoint["critic_model_state_dict"]
+            )
 
         self.critic_model.to(self.params["device"])
 
     def train_generator(self, batch):
-        if self.params['data']['drop_weights']:
+        if self.params["data"]["drop_weights"]:
             x, dlls = batch
         else:
             x, dlls, weight = batch
@@ -117,7 +137,12 @@ class WGAN():
         dlls = dlls.to(self.params["device"]).type(torch.float)
 
         noized_x = torch.cat(
-            [x, get_noise(x.shape[0], self.params['noise_dim']).to(self.params["device"])],
+            [
+                x,
+                get_noise(x.shape[0], self.params["noise_dim"]).to(
+                    self.params["device"]
+                ),
+            ],
             dim=1,
         )
 
@@ -126,14 +151,12 @@ class WGAN():
 
         generator_loss = -torch.mean(crit_fake_pred)
 
-        generator_result = {
-            'G/loss' : generator_loss
-        }
+        generator_result = {"G/loss": generator_loss}
 
         return generator_result
 
     def train_critic(self, batch):
-        if self.params['data']['drop_weights']:
+        if self.params["data"]["drop_weights"]:
             x, dlls = batch
         else:
             x, dlls, weight = batch
@@ -141,7 +164,12 @@ class WGAN():
         dlls = dlls.to(self.params["device"]).type(torch.float)
 
         noized_x = torch.cat(
-            [x, get_noise(x.shape[0], self.params['noise_dim']).to(self.params["device"])],
+            [
+                x,
+                get_noise(x.shape[0], self.params["noise_dim"]).to(
+                    self.params["device"]
+                ),
+            ],
             dim=1,
         )
 
@@ -151,29 +179,34 @@ class WGAN():
         crit_fake_pred = self.critic_model(generated.detach())
         crit_real_pred = self.critic_model(real_full)
 
-        epsilon = torch.rand(real_full.size(0), 1, 1, 1, device=device, requires_grad=True)
-        gradient = get_gradient(self.critic_model, real_full, generated.detach(), epsilon=epsilon)
+        epsilon = torch.rand(
+            real_full.size(0), 1, 1, 1, device=device, requires_grad=True
+        )
+        gradient = get_gradient(
+            self.critic_model, real_full, generated.detach(), epsilon=epsilon
+        )
         gp = gradient_penalty(gradient)
-        crit_loss = torch.mean(crit_fake_pred - crit_real_pred) + self.params['c_lambda'] * gp
+        crit_loss = (
+            torch.mean(crit_fake_pred - crit_real_pred) + self.params["c_lambda"] * gp
+        )
 
-        critic_result = {
-            'C/loss' : critic_loss,
-            'gradient_penalty': gp
-        }
+        critic_result = {"C/loss": critic_loss, "gradient_penalty": gp}
 
         return critic_result
 
     @torch.no_grad()
     def generate(self, batch):
-        if self.params['data']['drop_weights']:
+        if self.params["data"]["drop_weights"]:
             x, dlls = batch
         else:
             x, dlls, weight = batch
-            
+
         x = x.to(self.params["device"]).type(torch.float)
         dlls = dlls.to(self.params["device"]).type(torch.float)
 
-        noize = get_noise(x.shape[0], self.params['noise_dim']).to(self.params["device"])
+        noize = get_noise(x.shape[0], self.params["noise_dim"]).to(
+            self.params["device"]
+        )
         noized_x = torch.cat(
             [x, noize],
             dim=1,
@@ -181,11 +214,10 @@ class WGAN():
         generated = self.generator_model(noized_x)
         return generated
 
-
     def get_histograms(self, generated, real):
-        features_names = self.params['features_names']
+        features_names = self.params["features_names"]
         num_of_subplots = len(features_names)
-        
+
         plt.clf()
         fig, axes = plt.subplots(num_of_subplots, 1, figsize=(10, 16))
         plt.suptitle("Histograms")
@@ -225,23 +257,14 @@ class WGAN():
     def validation_step(self, generated, real, weights=None):
         figure = self.get_histograms(generated, real)
         roc_auc_score = self.get_roc_auc_score(generated, real)
-        outputs = {
-            'histogram': figure,
-            'rocauc': roc_auc_score
-        }
+        outputs = {"histogram": figure, "rocauc": roc_auc_score}
         return outputs
-
 
     def get_roc_auc_score(self, generated, real):
         X = np.concatenate((generated, real))
         y = np.array([0] * generated.shape[0] + [1] * real.shape[0])
 
-        (
-            X_train,
-            X_test,
-            y_train,
-            y_test,
-        ) = train_test_split(
+        (X_train, X_test, y_train, y_test,) = train_test_split(
             X,
             y,
             test_size=0.2,
